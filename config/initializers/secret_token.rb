8ff91c76d1a97a37c915fa4d006e0089a58843a7
Alexis Matherlee <alexismatherlee@gmail.com>
2014-02-13 23:40:06 -0800

    captured kittens

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scaffolding::Application.config.secret_key_base = 'eb117660041af67a1eed525ba29dec04ed8360681ebdc009c1d160aea8c00d0853cc528280bd534f88ef2607c232d132fb709e5226b9ed6cf688bd70018685f3'
